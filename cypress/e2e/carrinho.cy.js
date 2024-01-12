48630ef761c93d6c75a802e9027ea769e12bf434
Aline Serafim <aline.serafim86@gmail.comgit config --global user.name Aline>
2024-01-12 16:06:01 -0300

    alterações no login e carrinho

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -7,49 +7,9 @@ describe('Adicionar item ao carrinho', () => {
         cy.visit('produtos')
     });
 
-    it('Não é permitido inserir mais de 10 itens de um mesmo produto ao carrinho', () => {
-        cy.get('.product-block')
-            .contains('Abominable Hoodie').click()
-            cy.get('.button-variable-item-S').click()
-            cy.get('.button-variable-item-Green').click()
-            cy.get('.input-text').clear().type(10)
-            cy.get('.single_add_to_cart_button').click()
 
-            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 10)
-            cy.get('.woocommerce-message').should('contain', '10 × “Abominable Hoodie” foram adicionados no seu carrinho.')
-    });
-
-    it('Os valores não podem ultrapassar a R$ 990,00', () => {
+    it('Adicionar produtos no carrinho.', () => {
         ProdutosJson.comprarTresprodutos()
     })
     
-
-    it('Valores entre R$ 200,00 e R$ 600,00, ganham cupom de 10%', () => {
-        cy.get('.post-2559 > .product-block')
-            .contains('Abominable Hoodie').click();
-        
-        cy.get('.button-variable-item-S').click();
-        cy.get('.button-variable-item-Green').click();  
-        cy.get('.input-text').clear().type(6);
-        
-        cy.get('.single_add_to_cart_button').click();
-        cy.get('.woocommerce-message').should('contain', '6 × “Abominable Hoodie” foram adicionados no seu carrinho.');
-    
-        cy.get('.sub-title').should('contain', '414');
-    });
-
-
-    it('Valores acima de R$ 600,00 ganham cupom de 15%', () => {
-        cy.get('.post-2559 > .product-block')
-            .contains('Abominable Hoodie').click();
-        
-        cy.get('.button-variable-item-S').click();
-        cy.get('.button-variable-item-Green').click();  
-        cy.get('.input-text').clear().type(11);
-        
-        cy.get('.single_add_to_cart_button').click();
-        cy.get('.woocommerce-message').should('contain', '11 × “Abominable Hoodie” foram adicionados no seu carrinho.');
-    
-        cy.get('.sub-title').should('contain', '759');
-    });
 });
--- cypress/support/json-produtos/produtos.json.js
+++ cypress/support/json-produtos/produtos.json.js
@@ -7,26 +7,22 @@ class ProdutosJson {
         
         cy.get('.button-variable-item-S').click();
         cy.get('.button-variable-item-Green').click();  
-        cy.get('.input-text').clear().type(10);
+        cy.get('.input-text').clear().type(2);
         
         cy.get('.single_add_to_cart_button').click();
 
         cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2)').click();
-
-        // cy.get('.woocommerce-message').should('contain', '10 × “Abominable Hoodie” foram adicionados no seu carrinho.');
         
         //SEGUNDO PRODUTO
         cy.get('.post-3111 > .product-block').click();
 
         cy.get('.button-variable-item-S').click();
         cy.get('.button-variable-item-Black').click();  
-        cy.get('.input-text').clear().type(6);
+        cy.get('.input-text').clear().type(2);
         
         cy.get('.single_add_to_cart_button').click();
 
         cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click();
-
-        // cy.get('.woocommerce-message').should('contain', '6 × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.');
 
         //TERCEIRO PRODUTO
         cy.get('.post-3073 > .product-block').click();
@@ -39,7 +35,7 @@ class ProdutosJson {
 
         cy.get('.woocommerce-message').should('contain', '2 × “Aether Gym Pant” foram adicionados no seu carrinho.');
         
-        cy.get('.sub-title').should('contain', '982');
+        cy.get('.sub-title').should('contain', '334');
 
     }
 }
